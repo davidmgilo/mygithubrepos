5d417683290072aaa8d9ea1dc87846d4c32f443c
David Martinez <davidmgilo@gmail.com>
2016-11-15 19:27:45 +0100

    added blade for mygithubrepos

---
+++ resources/views/mygithubrepos.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    My github repos
+@endsection
+
+
+@section('main-content')
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">My Repos</div>
+
+                    <div class="panel-body">
+                        TODO: here llista de repos
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
