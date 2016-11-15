f2c25d2f1a4f96bf0279d27bc519db6713f2d811
David Martinez <davidmgilo@gmail.com>
2016-11-15 19:24:35 +0100

    modified sidebar && created route

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -34,13 +34,14 @@
             <li class="header">Github</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>Github</span> <i class="fa fa-angle-left pull-right"></i></a>
-                <ul class="treeview-menu">
-                    <li><a href="#">MyGithub repos</a></li>
+            <li class="active"><a href="{{ url('mygithubrepos') }}"><i class='fa fa-link'></i> <span>MyGithub repos</span></a></li>
+            {{--<li class="treeview">--}}
+                {{--<a href="#"><i class='fa fa-link'></i> <span>Github</span> <i class="fa fa-angle-left pull-right"></i></a>--}}
+                {{--<ul class="treeview-menu">--}}
+                    {{--<li><a href="{{ url('mygithubrepos') }}">MyGithub repos</a></li>--}}
 
-                </ul>
-            </li>
+                {{--</ul>--}}
+            {{--</li>--}}
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
